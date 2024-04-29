1a2c9fd56a9108058d855800f93b45f51db262a5
JacquelinD <doranjacquelin@gmail.com>
2024-04-29 16:09:18 -0400

    added in blog posts template

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Posts from "./components/Posts";
 
 function App() {
   return (
-    <div className="App">
+    <div className="main-container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1 className="main-header">
+          Blog App using React JS
+        </h1>
       </header>
+      <Posts />
     </div>
   );
 }
