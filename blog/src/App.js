7619f5daa2bef04d63e8b54d526eb4325b01b877
JacquelinD <doranjacquelin@gmail.com>
2024-04-29 16:53:34 -0400

    remove logos

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,27 +1,37 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
 import Posts from "./components/Posts";
 
+const mode = {
+  cute: {background: '#F3EAD6', 
+         text: '#A66d45', 
+         headings: '#F6B6A7', 
+         links: '#FD9584', 
+         misc: '#A7A155', 
+         misc2: '#D2C98A'}
+}
+
 function App() {
+  const style = mode[0]
   return (
-    <div className="main-container">
+    <div className="main-container" style={style}>
+      {/* <script src="../public/colorTheme.js" />
+      <style jsx global>
+        {`
+        .fun-theme{
+          --headings: #D2C98A;
+          --text: #A66D45;
+          --link: #FD9584;
+        }
+        `}
+        </style> */}
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <nav>
+        </nav>
         <h1 className="main-header">
           Blog App using React JS
+          your preference is {style}
         </h1>
       </header>
       <Posts />
