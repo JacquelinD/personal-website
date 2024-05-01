ae4cf827aa44d2bc99d2ef70426384baa82b3019
JacquelinD <doranjacquelin@gmail.com>
2024-05-01 11:39:00 -0400

    update links

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,38 +3,35 @@ import './App.css';
 
 import Posts from "./components/Posts";
 
-const mode = {
-  cute: {background: '#F3EAD6', 
-         text: '#A66d45', 
-         headings: '#F6B6A7', 
-         links: '#FD9584', 
-         misc: '#A7A155', 
-         misc2: '#D2C98A'}
-}
+// const mode = {
+//   cute: {background: '#F3EAD6', 
+//          text: '#A66d45', 
+//          headings: '#F6B6A7', 
+//          links: '#FD9584', 
+//          misc: '#A7A155', 
+//          misc2: '#D2C98A'}
+// }
 
 function App() {
-  const style = mode[0]
   return (
-    <div className="main-container" style={style}>
-      {/* <script src="../public/colorTheme.js" />
-      <style jsx global>
-        {`
-        .fun-theme{
-          --headings: #D2C98A;
-          --text: #A66D45;
-          --link: #FD9584;
-        }
-        `}
-        </style> */}
-      <header className="App-header">
-        <nav>
-        </nav>
-        <h1 className="main-header">
+    <div className="main-container">
+      <nav className="main-nav">
+        <ul>
+          <li>Test 1</li>
+          <li>Link 1</li>
+          <li>Link to about me page</li>
+        </ul>
+          {/* Add in a hamburger here */}
+      </nav>
+      <header className="main-header">
+        <h1>
           Blog App using React JS
-          your preference is {style}
         </h1>
       </header>
       <Posts />
+      <footer>
+        {/* Social links */}
+      </footer>
     </div>
   );
 }
