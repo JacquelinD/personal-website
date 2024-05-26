eebbe4cfabe77f825b1d479e3228902dd701dd53
JacquelinD <doranjacquelin@gmail.com>
2024-05-26 13:50:57 -0400

    add in bones for homepage

--- blog/src/App.js
+++ blog/src/App.js
@@ -17,20 +17,40 @@ function App() {
     <div className="main-container">
       <nav className="main-nav">
         <ul>
-          <li>Test 1</li>
-          <li>Link 1</li>
-          <li>Link to about me page</li>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>About Me</li>
         </ul>
           {/* Add in a hamburger here */}
       </nav>
       <header className="main-header">
-        <h1>
-          Blog App using React JS
+        <h1 className="quote">
+          Who really needs the past with the alure of something new? 
         </h1>
       </header>
-      <Posts />
+      <div className="intro-container">
+        About Me & Photo
+      </div>
+      <div className="latest-blog-container">
+        Check out my latest Blog!
+      </div>
+      <div className="disc-container">
+        Disc Widget 
+      </div>
+      <div className="reading-container">
+        What I'm curently reading
+      </div>
+      {/* <Posts /> */}
       <footer>
         {/* Social links */}
+        <div className="socials-container">
+        <a href="" className="socails">
+          <img src="" alt="FB"/>
+        </a>
+        <a href="">
+          <img src="" alt="FB"/>
+        </a>
+        </div>
       </footer>
     </div>
   );
